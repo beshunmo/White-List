9a1d91cdb8850b483053b53c07d5bd03b5c560f4
le0n229 <leon229@yandex.ru>
2019-03-31 09:30:30 +0300

    upload server

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
+const fs = require('fs');
+const multiparty = require('multiparty');
 const Product = require('../models/product');
 const Shops = require('../models/shops');
 const Carts = require('../models/cart');
 const authenticationMiddlewareApi = require('../authentication/middleware');
+
 
 const router = express.Router();
 
@@ -79,6 +82,58 @@ router.get('/product/:id', async (req, res) => {
   console.log(product);
   res.status(200);
   res.send(product);
+});
+
+router.post('/upload', (req, res, next) => {
+  // create a form to begin parsing
+  const form = new multiparty.Form();
+  const uploadFile = { uploadPath: '', type: '', size: 0 };
+  const maxSize = 20 * 1024 * 1024; // 20MB
+  const supportMimeTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+  const errors = [];
+
+  form.on('error', (err) => {
+    if (fs.existsSync(uploadFile.path)) {
+      fs.unlinkSync(uploadFile.path);
+      console.log('error');
+    }
+  });
+
+  form.on('close', () => {
+    if (errors.length == 0) {
+      res.send({ status: 'ok', text: 'Success' });
+    } else {
+      if (fs.existsSync(uploadFile.path)) {
+        fs.unlinkSync(uploadFile.path);
+      }
+      res.send({ status: 'bad', errors });
+    }
+  });
+
+  // listen on part event for image file
+  form.on('part', (part) => {
+    uploadFile.size = part.byteCount;
+    uploadFile.type = part.headers['content-type'];
+    uploadFile.path = `./files/${part.filename}`;
+
+    if (uploadFile.size > maxSize) {
+      errors.push(`File size is ${uploadFile.size / 1024 / 1024}. Limit is${maxSize / 1024 / 1024}MB.`);
+    }
+
+    if (supportMimeTypes.indexOf(uploadFile.type) == -1) {
+      errors.push(`Unsupported mimetype ${uploadFile.type}`);
+    }
+
+    if (errors.length == 0) {
+      const out = fs.createWriteStream(uploadFile.path);
+      part.pipe(out);
+    } else {
+      part.resume();
+    }
+  });
+
+  // parse the form
+  form.parse(req);
 });
 
 export default router;
